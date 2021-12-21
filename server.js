eac7044f426c89b80cc3b44421b3a158c6e19d61
Emin Gürer <emin.gurer45@gmail.com>
2021-12-21 18:27:11 +0300

    Create and Update routes implemented

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 8080;
+const methodOverride = require('method-override');
 
 const DB_URL = 'mongodb://localhost:27017/farmStand';
 const mongoose = require('mongoose');
@@ -19,20 +20,52 @@ mongoose
 //Body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 //View engine and static assets
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //Routes
+
+//Products
 app.get('/', async (req, res) => {
   const products = await Product.find({});
-  res.render('products/index.ejs', { products });
+  res.render('products/products.ejs', { products });
 });
-app.get('/products/:id', async (req, res) => {
+app.get('/products/id=:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render('products/details.ejs', { product });
+});
+
+//Create
+app.get('/products/create', (req, res) => {
+  res.render('products/create.ejs');
+});
+app.post('/products/create', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/id=${newProduct._id}`);
+});
+
+//Update
+app.get('/products/id=:id/update', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render(`products/update.ejs`, { product });
+});
+app.put('/products/id=:id/update', async (req, res) => {
+  const { id } = req.params;
+  let product = {};
+  try {
+    product = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  res.redirect(`/products/id=${product.id}`);
 });
 
 //Route fallback
