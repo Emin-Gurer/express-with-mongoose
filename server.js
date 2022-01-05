a58aa439ecc2c9322319a56d12a43008da2a893b
Emin Gürer <emin.gurer45@gmail.com>
2022-01-05 18:52:35 +0300

    Error handler class is added, async error hadle utility added

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require('path');
 const port = 8080;
 const methodOverride = require('method-override');
-
+const AppError = require('./AppError');
+const wrapAsync = require('./errorUtilities/asyncWrap');
 const DB_URL = 'mongodb://localhost:27017/farmStand';
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const { wrap } = require('module');
 mongoose
   .connect(DB_URL)
   .then(() => {
@@ -29,58 +31,82 @@ app.set('views', path.join(__dirname, 'views'));
 //Routes
 
 //Products
-app.get('/', async (req, res) => {
-  const products = await Product.find({});
-  res.render('products/products.ejs', { products });
-});
-app.get('/products/id=:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render('products/details.ejs', { product });
-});
+app.get(
+  '/',
+  wrapAsync(async (req, res) => {
+    const products = await Product.find({});
+    res.render('products/products.ejs', { products });
+  })
+);
+app.get(
+  '/products/id=:id',
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      //You have to pass error to next if error is coming from async promise
+      throw new AppError('Product is not found', 404);
+    }
+    res.render('products/details.ejs', { product });
+  })
+);
 
 //Create
 app.get('/products/create', (req, res) => {
   const categories = Product.schema.path('category').enumValues;
   res.render('products/create.ejs', { categories });
 });
-app.post('/products/create', async (req, res) => {
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  res.redirect(`/products/id=${newProduct._id}`);
-});
+app.post(
+  '/products/create',
+  wrapAsync(async (req, res, next) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/id=${newProduct._id}`);
+  })
+);
 
 //Update
-app.get('/products/id=:id/update', async (req, res) => {
-  const { id } = req.params;
-  const categories = Product.schema.path('category').enumValues;
-  const product = await Product.findById(id);
-  res.render(`products/update.ejs`, { product, categories });
-});
-app.put('/products/id=:id/update', async (req, res) => {
-  const { id } = req.params;
-  let product = {};
-  try {
+app.get(
+  '/products/id=:id/update',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const categories = Product.schema.path('category').enumValues;
+    const product = await Product.findById(id);
+    res.render(`products/update.ejs`, { product, categories });
+  })
+);
+app.put(
+  '/products/id=:id/update',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    let product = {};
     product = await Product.findByIdAndUpdate(id, req.body, {
       runValidators: true,
       new: true,
     });
-  } catch (err) {
-    console.log(err);
-  }
-  res.redirect(`/products/id=${product.id}`);
-});
+    res.redirect(`/products/id=${product.id}`);
+  })
+);
 
 //Delete
-app.delete('/products/id=:id/delete', async (req, res) => {
-  const { id } = req.params;
-  const response = await Product.findByIdAndDelete(id);
-  res.redirect('/');
-});
+app.delete(
+  '/products/id=:id/delete',
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const response = await Product.findByIdAndDelete(id);
+    res.redirect('/');
+  })
+);
 
 //Route fallback
 app.get('*', (req, res) => {
   res.send('This page doesnt exist');
+});
+//Error handler middleware
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Something went wrong' } = err;
+  console.log(err);
+  res.status(status).send(message);
 });
 //Start server
 app.listen(port, (req, res) => {
