7b3fa5076bc7a62974272d13420ce16205dcb536
Emin Gürer <emin.gurer45@gmail.com>
2022-01-11 17:48:54 +0300

    Products styled as cards

--- models/product.js
+++ models/product.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Product must have a name'],
   },
   price: {
     type: Number,
-    required: true,
+    required: [true, 'Product must have a price'],
     min: 0,
+  },
+  image: {
+    type: String,
+    required: [true, 'Product must have an image url'],
   },
   category: {
     type: String,
     enum: ['Fruit', 'Vegetable', 'Dairy'],
+    required: [true, 'Product must have a category'],
   },
 });
 
